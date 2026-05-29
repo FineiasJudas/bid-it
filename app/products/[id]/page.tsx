9bb50b46daa81bcc8899f3d074bb98414b84203c
Fineias Judas <judasfineias@gmail.com>
2026-05-29 15:36:03 +0100

    Pagina de um produto individual 5

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -15,10 +15,10 @@ export default function ProductPage({ params }: { params: { id: string } })
   }
 
   return (
-    <div className="flex flex-col items-center px-14 py-8 mt-10 mb-20">
+    <div className="flex flex-col items-center px-14 py-8 mt-10 mb-20 bg-[#efefef]">
         <h1 className="text-3xl text-gray-700 font-bold mb-8">Produto ativo</h1>
       <div className="flex flex-col lg:flex-row gap-10">
-      <div className="w-100 flex flex-col bg-white rounded-lg shadow-md p-4 gap-8">
+      <div className="w-80 md:w-100 flex flex-col bg-white rounded-lg shadow-md p-4 gap-8">
        <div className="flex px-2 items-center rounded-md">
           <Image 
             src={"/6011.jpg"}
@@ -41,12 +41,12 @@ export default function ProductPage({ params }: { params: { id: string } })
           placeholder="Coloque um valor"
           className="bg-gray-100 px-4 py-2 rounded-lg border border-gray-300 mb-3"
         />
-        <button onClick={() => console.log("Bid:", bidAmount)} className="bg-gray-800 rounded text-white p-2">
+        <button onClick={() => console.log("Bid:", bidAmount)} className="bg-gray-800 rounded text-white p-2 mb-3">
           Fazer Bid
         </button>
       </div>
       </div>
-      <div className="w-100 h-60 flex flex-col bg-white rounded-lg shadow-md p-6">
+      <div className="w-80 md:w-100 h-60 flex flex-col bg-white rounded-lg shadow-md p-6">
         <h1 className="text-gray-800 text-2xl mb-4">Comentar</h1>
           <form onSubmit={commentSubmition} className="flex flex-col gap-5">
              <textarea
@@ -58,7 +58,7 @@ export default function ProductPage({ params }: { params: { id: string } })
              <input type="submit" value={"Enviar"} className="px-2 py-2 rounded bg-gray-800 text-white"/>
           </form>
       </div>
-      <div className="w-100 flex flex-col bg-white rounded-lg shadow-md p-6">
+      <div className="w-80 md:w-100 flex flex-col bg-white rounded-lg shadow-md p-6">
         <h1 className="text-gray-800 text-2xl mb-4">Comentários</h1>
         <div className="flex flex-col border-1 border-gray-300 rounded-2xl px-2 py-4 bg-gray-100">
           <p className="text-1xl text-gray-700 font-bold ml-2">@Author</p>
